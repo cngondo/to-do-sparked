7f99127eba0ac53ea830fd609780addeee7d0f70
cngondo <devngondo@gmail.com>
2017-02-13 19:49:09 +0300

    added getter for description

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -5,11 +5,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TaskTest {
+	Task myTask;
 
 	@Test
 	public void Task_instantiatesCorrectly_true(){
 		Task myTask = new Task("Mow the lawn");
 	    assertEquals(true, myTask instanceof Task);
 	}
-   
+
+   @Test
+   public void Task_instantiatesWithDescription_String(){
+   		new Task("Mow the Lawn");
+	    assertEquals("Mow the lawn", myTask.getDescription());
+
+   }
 }
