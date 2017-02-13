2576e934a5aebbe2989a8e7a836bc903be5c76b1
cngondo <devngondo@gmail.com>
2017-02-13 20:00:55 +0300

    added index route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
     public static void main(String[] args) {
     	staticFileLocation("/public");
 	    String layout = "templates/layout.vtl";
+
+	    //the root route loading the index.vtl
+	    get("/", (request, response) -> {
+	    	Map<String, Object> model = new HashMap<String, Object>();
+	    	model.put("templates", "templates/index.vtl");
+	    	return new ModelAndView(model, layout);  	
+	    }, new VelocityTemplateEngine());
     }
 }
