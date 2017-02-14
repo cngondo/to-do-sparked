94d2425178f741d7cce7c3e7b9bf73c1a06893a8
cngondo <devngondo@gmail.com>
2017-02-14 05:20:40 +0300

    display one task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,8 +12,26 @@ public class App {
 	    //the root route loading the index.vtl
 	    get("/", (request, response) -> {
 	    	Map<String, Object> model = new HashMap<String, Object>();
+	    	//retrieve task session and placing it in the model
+	    	model.put("task", request.session.attribute("task"));	    	
 	    	model.put("templates", "templates/index.vtl");
 	    	return new ModelAndView(model, layout);  	
+	    }, new VelocityTemplateEngine());
+
+	    //the task route
+	    /*
+		* 
+	    */
+	    post("/task", (request, response) -> {
+	    	Map<String, Object> model = new HashMap<String, Object>();
+
+	    	String description = request.queryparams("description");
+	    	//save our task object to the user session
+	    	Task newTask = new Task(description);
+	    	request.session.attribute("task", newTask);
+
+	    	model.put("template","templates/success.vtl");
+	    	return new ModelAndView(model, layout);
 	    }, new VelocityTemplateEngine());
     }
 }
